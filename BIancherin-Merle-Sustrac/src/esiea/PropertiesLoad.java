13450e64ddd507958ffbe3d319ac3a8703b79f71
Jerome <jerome.merle.work@gmail.com>
2015-02-23 14:39:09 +0100

    Possibilité de sauvegarder les modifications des conf

--- BIancherin-Merle-Sustrac/src/esiea/PropertiesLoad.java
+++ BIancherin-Merle-Sustrac/src/esiea/PropertiesLoad.java
@@ -1,16 +1,19 @@
 package esiea;
 import java.util.Iterator;
 import java.util.Properties;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.FileInputStream;
+import java.io.OutputStream;
 
 /**
  * Prise en compte des options de conf contenu dans le fichier propertie
  * @param filename fichier où sont ecrits les propriétés
  */
 class PropertiesLoad {
-   protected static Properties load(String filename) throws IOException, FileNotFoundException{
+	static String propertiesFileLocation="myFile.properties";
+	protected static Properties load(String filename) throws IOException, FileNotFoundException{
       Properties properties = new Properties();
       FileInputStream input = new FileInputStream(filename);
       try{
@@ -22,7 +25,7 @@ class PropertiesLoad {
 
    	//Lis toute les valeurs contenues dans properties
 	protected static void displayProperties(Properties props) {
-		Iterator it = props.keySet().iterator();
+		Iterator<Object> it = props.keySet().iterator();
 		while (it.hasNext()) {
 			String propertyName = (String) it.next();
 			String propertyValue = props.getProperty(propertyName);
@@ -30,12 +33,24 @@ class PropertiesLoad {
 		}
 	}
 
+	//Ecris les nouvelles valeurs dans le fichier de conf
+	public static void saveProperties(Properties props, String fileLocation, String comments) throws FileNotFoundException,
+			IOException {
+		OutputStream out = new FileOutputStream(fileLocation);
+		props.store(out, comments);
+		out.flush();
+		out.close();
+	}
    public static void main(String[] args){
       try{
          // chargement des propriétés
          Properties prop = PropertiesLoad.load("myFile.properties");
          //parcourt et affiche les propriétes
          displayProperties(prop);
+         //modifie les propriété
+         prop.setProperty("level", "INFO");
+         //sauvegarde les modif dans le fichier
+         saveProperties(prop,propertiesFileLocation, null);
       }
       catch(Exception e){
          e.printStackTrace();
