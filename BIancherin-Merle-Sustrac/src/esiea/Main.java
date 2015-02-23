515c1b36bf99b59a476e3b2860b069a2387ccfd5
louis14448 <louis.biancherin@gmail.com>
2015-02-23 16:16:24 +0100

    Amelioration code

--- BIancherin-Merle-Sustrac/src/esiea/Main.java
+++ BIancherin-Merle-Sustrac/src/esiea/Main.java
@@ -1,36 +1,57 @@
 package esiea;
 
+import Logger.Logger;
+import Logger.LoggerFactory;
+
 import java.util.Properties;
-
-
-
 
 public class Main {
 
 	public static void main(String[] args) {
-		 Logger logger = LoggerFactory.getLogger(Main.class);
-		   logger.debug("Test debug ESIEA");
-		   logger.info("Test info ESIEA");
-		   logger.error("Test error ESIEA");
-		  System.out.println(logger); 
-		 // Folder folder=new Folder(10,"files01","10/12/2014");
-		  
-		  /***TEST FILE CONF***/
-		  PropertiesFiles propri = new PropertiesFiles();
-		  try{
-		         // chargement des propriétés
-		         Properties prop = PropertiesFiles.load("myFile.properties");
-		         //parcourt et affiche les propriétes
-		         propri.displayProperties(prop);
-		         //modifie les propriété
-		         prop.setProperty("level", "INFO");
-		         //sauvegarde les modif dans le fichier
-		         propri.saveProperties(prop,propri.propertiesFileLocation, null);
-		      }
-	      catch(Exception e){
-	          e.printStackTrace();
-	       }
-		 
+		Logger logger = LoggerFactory.getLogger(Main.class);
+		logger.debug("Test debug ESIEA");
+		logger.info("Test info ESIEA");
+		logger.error("Test error ESIEA");
+		logger.debug("Test debug ESIEA");
+		logger.info("Test info ESIEA");
+		logger.error("Test error ESIEA");
+		logger.debug("Test debug ESIEA");
+		logger.info("Test info ESIEA");
+		logger.error("Test error ESIEA");
+		logger.debug("Test debug ESIEA");
+		logger.info("Test info ESIEA");
+		logger.error("Test error ESIEA");
+		logger.debug("Test debug ESIEA");
+		logger.info("Test info ESIEA");
+		logger.error("Test error ESIEA");
+		logger.debug("Test debug ESIEA");
+		logger.info("Test info ESIEA");
+		logger.error("Test error ESIEA");
+		logger.debug("Test debug ESIEA");
+		logger.info("Test info ESIEA");
+		logger.error("Test error ESIEA");
+		logger.debug("Test debug ESIEA");
+		logger.info("Test info ESIEA");
+		logger.error("Test error ESIEA");
+		// System.out.println(logger);
+		// Folder folder=new Folder(10,"files01","10/12/2014");
+
+		/*** TEST FILE CONF ***/
+		// PropertiesFiles propri = new PropertiesFiles();
+		try {
+			// chargement des propriï¿½tï¿½s
+			Properties prop = Logger.loadProperties("myFile.properties");
+			// parcourt et affiche les propriï¿½tes
+			Logger.displayProperties(prop);
+			// modifie les propriï¿½tï¿½
+			prop.setProperty("level", "INFO");
+			// sauvegarde les modif dans le fichier
+			Logger.saveProperties(prop);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
