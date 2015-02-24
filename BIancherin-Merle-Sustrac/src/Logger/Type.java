f68208433a3eda4853ab7949fbe07b7a83fcd1e5
Sid29 <estephesustrac@10.8.95.186>
2015-02-24 22:58:24 +0100

    Ajout de la recherche d'un type en fonction de sa description

--- BIancherin-Merle-Sustrac/src/Logger/Type.java
+++ BIancherin-Merle-Sustrac/src/Logger/Type.java
@@ -1,5 +1,29 @@
 package Logger;
 
 public enum Type {
-	DEBUG,INFO,ERROR 
+
+	DEBUG("Debug"),INFO("Info"),ERROR("Erreur");
+	
+		private String name;
+
+		private Type(String name) {
+			this.name = name;
+		}
+		
+		
+		/**
+		 * Récupération d'un type en fonction de sa description
+		 * 
+		 * @param texte
+		 *            du message
+		 */
+		public static Type getType(String name){
+			for(Type type : Type.values()){
+				if(type.name.toUpperCase().equals(name.toUpperCase())){
+					return type;
+				}
+			}
+			return Type.ERROR;
+		}
+			
 }
