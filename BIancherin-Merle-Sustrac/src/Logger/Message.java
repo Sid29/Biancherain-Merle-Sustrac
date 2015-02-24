b5feb689542789f2e304fd9fe75efb9606e792d4
Sid29 <estephesustrac@10.8.95.186>
2015-02-24 23:00:48 +0100

    Fonction isShow pour savoir si on doit afficher le type avec le niveau selectionner dans la propriété

--- BIancherin-Merle-Sustrac/src/Logger/Message.java
+++ BIancherin-Merle-Sustrac/src/Logger/Message.java
@@ -1,10 +1,8 @@
 package Logger;
 
-
 import java.util.Date;
 
-
-public class Message implements Comparable<Message>  {
+public class Message implements Comparable<Message> {
 
 	private Date date;
 	private String nom;
@@ -14,27 +12,37 @@ public class Message implements Comparable<Message>  {
 	public Message(String nom, String description, Type type) {
 		this.nom = nom;
 		this.description = description;
-		this.type =type;
+		this.type = type;
 		this.date = new java.util.Date();
 	}
-	
-	
 
 	@Override
 	public String toString() {
-		String format = "yyyy-MM-dd HH:mm:ss.SSS"; 
-		java.text.SimpleDateFormat formater = new java.text.SimpleDateFormat( format ); 
+		String format = "yyyy-MM-dd HH:mm:ss.SSS";
+		java.text.SimpleDateFormat formater = new java.text.SimpleDateFormat(
+				format);
 
-		return formater.format( date )  + "[NAME=" + nom + " LEVEL="+type+" MESSAGE="+ description + "]";
+		return formater.format(date) + "[NAME=" + nom + " LEVEL=" + type
+				+ " MESSAGE=" + description + "]";
 	}
-
-
 
 	@Override
 	public int compareTo(Message autreMesssage) {
-		
+
 		return this.type.compareTo(autreMesssage.type);
 	}
-
 	
-} 
+	/**
+	 * Verification de l'affichage du messsage en fonction de son type
+	 * 
+	 * @param type de la propriété
+	 */
+	public boolean isShow(Type typeLevel) {
+		System.out.println(typeLevel);
+		if (type.ordinal() < typeLevel.ordinal()) {
+			return false;
+		}
+		return true;
+	}
+
+}
