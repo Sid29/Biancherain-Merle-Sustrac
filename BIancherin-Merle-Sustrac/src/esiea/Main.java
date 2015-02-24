d05a2c707e812cdd54ca1f99de18ca07a12bfc77
Jerome <jerome.merle.work@gmail.com>
2015-02-24 14:01:06 +0100

    Modification de l'ordre des tests

--- BIancherin-Merle-Sustrac/src/esiea/Main.java
+++ BIancherin-Merle-Sustrac/src/esiea/Main.java
@@ -8,6 +8,22 @@ import java.util.Properties;
 public class Main {
 
 	public static void main(String[] args) {
+		
+
+		/*** TEST FILE CONF ***/
+		// PropertiesFiles propri = new PropertiesFiles();
+		try {
+			// chargement des propri�t�s
+			Logger.loadProperties("myFile.properties");
+			// parcourt et affiche les propri�tes
+			Logger.displayProperties();
+
+			// sauvegarde les modif dans le fichier
+			Logger.saveProperties("level", "INFO");
+			} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		Logger logger = LoggerFactory.getLogger(Main.class);
 		logger.debug("Test debug ESIEA");
 		logger.info("Test info ESIEA");
@@ -35,21 +51,6 @@ public class Main {
 		logger.error("Test error ESIEA");
 		// System.out.println(logger);
 		// Folder folder=new Folder(10,"files01","10/12/2014");
-
-		/*** TEST FILE CONF ***/
-		// PropertiesFiles propri = new PropertiesFiles();
-		try {
-			// chargement des propri�t�s
-			Logger.loadProperties("myFile.properties");
-			// parcourt et affiche les propri�tes
-			Logger.displayProperties();
-
-			// sauvegarde les modif dans le fichier
-			Logger.saveProperties("level", "INFO");
-			Logger.DisplayOnePropertie("level");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 
 }
